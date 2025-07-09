6c338e8d020e276ca6086705a6894eafdd0bc91a
murugesh-vp <murugesh.vp@gmail.com>
2025-07-09 11:47:49 +0530

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,9 @@
 // add delayed functionality here
+fetch('https://d2livegshl4cvv.cloudfront.net/seo/seo-data.json')
+.then(response => response.text())
+.then(structuredDataText => {
+  const script = document.createElement('script');
+  script.setAttribute('type', 'application/ld+json');
+  script.textContent = structuredDataText;
+  document.head.appendChild(script);
+});

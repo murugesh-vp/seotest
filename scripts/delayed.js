f695f3deadb1d01258c772caa29aa49565e4f4df
murugesh-vp <murugesh.vp@gmail.com>
2025-07-09 12:08:23 +0530

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,9 +1,16 @@
 // add delayed functionality here
-fetch('https://d2livegshl4cvv.cloudfront.net/seo/seo-data.json')
-.then(response => response.text())
-.then(structuredDataText => {
-  const script = document.createElement('script');
-  script.setAttribute('type', 'application/ld+json');
-  script.textContent = structuredDataText;
-  document.head.appendChild(script);
-});
+async function populateSeoData() {
+  try {
+    fetch('https://d2livegshl4cvv.cloudfront.net/seo/seo-data.json')
+    .then(response => response.text())
+    .then(structuredDataText => {
+      const script = document.createElement('script');
+      script.setAttribute('type', 'application/ld+json');
+      script.textContent = structuredDataText;
+      document.head.appendChild(script);
+    });
+  } catch (error) {
+    console.error('Error fetching seo data:', error);
+  }
+}
+populateSeoData();
